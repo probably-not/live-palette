7374a871d756af43d0f355af4775ea1dbb612e3c
Coby Benveniste <coby.benveniste@gmail.com>
2024-11-14 17:59:44 +0200

    Add the default icon component and pass the icon component in to the Palette

---
+++ lib/live_palette/icon.ex
@@ -0,0 +1,35 @@
+defmodule LivePalette.Icon do
+  @moduledoc """
+  `LivePalette.Icon` is the default icon component used to render icons in the LivePalette results.
+  It is a copy of the generated CoreComponent for icon that is added in Phoenix's project generation.
+  """
+
+  use Phoenix.Component
+
+  @doc """
+  Renders a [Heroicon](https://heroicons.com).
+
+  Heroicons come in three styles – outline, solid, and mini.
+  By default, the outline style is used, but solid and mini may
+  be applied by using the `-solid` and `-mini` suffix.
+
+  You can customize the size and colors of the icons by setting
+  width, height, and background color classes.
+
+  Icons are extracted from the `deps/heroicons` directory and bundled within
+  your compiled app.css by the plugin in your `assets/tailwind.config.js`.
+
+  ## Examples
+
+      <.icon name="hero-x-mark-solid" />
+      <.icon name="hero-arrow-path" class="ml-1 w-3 h-3 animate-spin" />
+  """
+  attr :name, :string, required: true
+  attr :class, :string, default: nil
+
+  def icon(%{name: "hero-" <> _} = assigns) do
+    ~H"""
+    <span class={[@name, @class]} />
+    """
+  end
+end
